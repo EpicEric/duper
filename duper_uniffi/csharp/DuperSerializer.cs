cb9011780ecfa44aab8f43a57f895b6dbbb27f20
Eric Rodrigues Pires <eric@eric.dev.br>
2025-11-12 17:23:28 -0300

    Start cementing the public API

---
+++ duper_uniffi/csharp/DuperSerializer.cs
@@ -0,0 +1,150 @@
+﻿namespace Duper;
+
+using Ffi;
+
+public class DuperSerializer
+{
+  public static T? Deserialize<T>(string @input)
+  {
+    var parsed = Duper.Parse(input, true);
+    return DeserializeInner<T>(parsed);
+  }
+
+  private static T? DeserializeInner<T>(DuperValue duperValue)
+  {
+    var t = typeof(T);
+    if (duperValue is DuperValue.Null)
+    {
+      if (!t.IsValueType || Nullable.GetUnderlyingType(t) != null)
+      {
+        // Returns null for reference types and nullable value types
+        return default;
+      }
+      throw new ApplicationException($"Cannot cast null to non-nullable {t}");
+    }
+    else if (duperValue is DuperValue.Object obj)
+    {
+      throw new ApplicationException("Unimplemented");
+    }
+    else if (duperValue is DuperValue.Array array)
+    {
+      throw new ApplicationException("Unimplemented");
+    }
+    else if (duperValue is DuperValue.Tuple tuple)
+    {
+      throw new ApplicationException("Unimplemented");
+    }
+    else if (duperValue is DuperValue.String str)
+    {
+      if (typeof(string).IsAssignableTo(t))
+      {
+        return (T)(object)str.value;
+      }
+      throw new ApplicationException($"Cannot cast string to {t}");
+    }
+    else if (duperValue is DuperValue.Bytes bytes)
+    {
+      if (typeof(byte[]).IsAssignableTo(t))
+      {
+        return (T)(object)bytes.value;
+      }
+      throw new ApplicationException($"Cannot cast bytes to {t}");
+    }
+    else if (duperValue is DuperValue.Temporal temporal)
+    {
+      if (typeof(string).IsAssignableTo(t))
+      {
+        return (T)(object)temporal.value;
+      }
+      else if (typeof(DateTime).IsAssignableTo(t))
+      {
+        // TO-DO: Validate the identifier first
+        return (T)(object)DateTime.Parse(temporal.value);
+      }
+      else if (typeof(DateTimeOffset).IsAssignableTo(t))
+      {
+        // TO-DO: Validate the identifier first
+        return (T)(object)DateTimeOffset.Parse(temporal.value);
+      }
+      throw new ApplicationException($"Cannot cast temporal to {t}");
+    }
+    else if (duperValue is DuperValue.Integer integer)
+    {
+      if (typeof(long).IsAssignableTo(t))
+      {
+        return (T)(object)integer.value;
+      }
+      else if (typeof(int).IsAssignableTo(t))
+      {
+        return (T)(object)(int)integer.value;
+      }
+      else if (typeof(double).IsAssignableTo(t))
+      {
+        return (T)(object)(double)integer.value;
+      }
+      else if (typeof(float).IsAssignableTo(t))
+      {
+        return (T)(object)(float)integer.value;
+      }
+      throw new ApplicationException($"Cannot cast integer to {t}");
+    }
+    else if (duperValue is DuperValue.Float flt)
+    {
+      if (typeof(double).IsAssignableTo(t))
+      {
+        return (T)(object)flt.value;
+      }
+      else if (typeof(float).IsAssignableTo(t))
+      {
+        return (T)(object)(float)flt.value;
+      }
+      throw new ApplicationException($"Cannot cast float to {t}");
+    }
+    else if (duperValue is DuperValue.Boolean boolean)
+    {
+      if (typeof(bool).IsAssignableTo(t))
+      {
+        return (T)(object)boolean.value;
+      }
+      throw new ApplicationException($"Cannot cast boolean to {t}");
+    }
+    else
+    {
+      throw new ApplicationException($"Unknown DuperValue type {duperValue.GetType()}");
+    }
+  }
+
+  public record SerializerOptions(
+      string? Indent,
+      bool StripIdentifiers,
+      bool Minify
+  )
+  { }
+
+  public static string Serialize<T>(T @value)
+  {
+    var duperValue = SerializeInner<T>(value);
+    return Duper.Serialize(duperValue, null);
+  }
+
+  public static string Serialize<T>(T? @value, SerializerOptions @options)
+  {
+    var duperValue = SerializeInner<T>(value);
+    return Duper.Serialize(duperValue, new SerializeOptions(options.Indent, options.StripIdentifiers, options.Minify));
+  }
+
+  private static DuperValue SerializeInner<T>(T? @value)
+  {
+    var t = typeof(T);
+    Attribute[] attrs = Attribute.GetCustomAttributes(t);
+    string? identifier;
+    foreach (Attribute attr in attrs)
+    {
+      if (attr is DuperAttribute a)
+      {
+        identifier = a.Identifier;
+      }
+    }
+    throw new ApplicationException("Unimplemented");
+  }
+}
--- duper_uniffi/example/csharp/Example.cs
+++ duper_uniffi/example/csharp/Example.cs
@@ -1,22 +1,29 @@
+using System.Text.Json;
 using Duper;
 
 namespace Example
 {
+  [Duper("UserProfile")]
   public class UserProfile
   {
+    [Duper("Uuid")]
     public required string @id;
     public required string @username;
+    [Duper("EmailAddress")]
     public required string @email;
     public required UserSettings @settings;
     public double @score;
+    [Duper("Png")]
     public required byte[] @avatar;
     public string? @bio;
-    public required IList<(string, string)> @last_logins;
+    public required IList<(string, DateTimeOffset)> @last_logins;
   }
 
   public class UserSettings
   {
+    [Duper(Key = "dark mode")]
     public bool DarkMode;
+    [Duper("Locale")]
     public required string @language;
     public Dictionary<string, string>? @metadata;
   }
@@ -25,25 +32,32 @@ namespace Example
   {
     public static void Main(string[] args)
     {
-      UserProfile? userProfile = DuperSerializer.Deserialize<UserProfile>(@"
-        UserProfile({
-          id: Uuid(""f111c275-b4ce-4392-8e5b-19067ce39b53""),
-          username: ""EpicEric"",
-          email: EmailAddress(""eric@duper.dev.br""),
-          settings: {
-            ""dark mode"": true,
-            language: Locale(""pt-BR""),
-            metadata: null,
-          },
-          score: 120.25,
-          // Support for bytes, woohoo!
-          avatar: Png(b64""iVBORw0KGgoAAAANSUhEUgAAAGQ""),
-          bio: r#""Hello! I'm a super ""duper"" user!""#,
-          last_logins: [
-            (IPv4Address(""192.168.1.100""), Instant('2024-03-20T14:30:00+00:00')),
-          ],
-        })");
-      Console.WriteLine(userProfile);
+      (string, string, float)? basic = DuperSerializer.Deserialize<(string, string, float)?>("""
+      ("1234", '2025-11', 123.4)
+      """);
+      Console.WriteLine(basic);
+
+      // UserProfile? userProfile = DuperSerializer.Deserialize<UserProfile>("""
+      //   UserProfile({
+      //     id: Uuid("f111c275-b4ce-4392-8e5b-19067ce39b53"),
+      //     username: "EpicEric",
+      //     email: EmailAddress("eric@duper.dev.br"),
+      //     settings: {
+      //       "dark mode": true,
+      //       language: Locale("pt-BR"),
+      //       metadata: null,
+      //     },
+      //     score: 120.25,
+      //     // Support for bytes, woohoo!
+      //     avatar: Png(b64"iVBORw0KGgoAAAANSUhEUgAAAGQ"),
+      //     bio: r#"Hello! I'm a super "duper" user!"#,
+      //     last_logins: [
+      //       (IPv4Address("192.168.1.100"), Instant('2024-03-20T14:30:00+00:00')),
+      //     ],
+      //   })
+      //   """);
+      // Console.WriteLine(userProfile);
+
       // Console.WriteLine(DuperSerializer.Serialize(userProfile));
     }
   }
