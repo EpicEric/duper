272508dad5f8459126bea1dc50cfe13eae39dc7f
Eric Rodrigues Pires <eric@eric.dev.br>
2025-11-16 23:29:09 -0300

    Use bundler settings for wasm-bindgen

--- duper-js-wasm/esbuild.js
+++ duper-js-wasm/esbuild.js
@@ -1,40 +1,40 @@
 import esbuild from "esbuild";
-import path from "node:path"
-import fs from "node:fs"
+import { wasmLoader } from "esbuild-plugin-wasm";
 
-// https://github.com/evanw/esbuild/issues/408#issuecomment-757555771
-const wasmPlugin = {
-  name: "wasm",
-  setup(build) {
-    build.onResolve({ filter: /\.wasm$/ }, args => {
-      if (args.resolveDir === "") {
-        return;
-      }
-      return {
-        path: path.isAbsolute(args.path) ? args.path : path.join(args.resolveDir, args.path),
-        namespace: "wasm-binary",
-      };
-    })
-
-    build.onLoad({ filter: /.*/, namespace: "wasm-binary" }, async (args) => ({
-      contents: await fs.promises.readFile(args.path),
-      loader: "binary",
-    }));
-  },
-};
+// // https://github.com/evanw/esbuild/issues/408#issuecomment-757555771
+// import path from "node:path"
+// import fs from "node:fs"
+// const wasmPlugin = {
+//   name: "wasm",
+//   setup(build) {
+//     build.onResolve({ filter: /\.wasm$/ }, args => {
+//       if (args.resolveDir === "") {
+//         return;
+//       }
+//       return {
+//         path: path.isAbsolute(args.path) ? args.path : path.join(args.resolveDir, args.path),
+//         namespace: "wasm-binary",
+//       };
+//     })
+//     build.onLoad({ filter: /.*/, namespace: "wasm-binary" }, async (args) => ({
+//       contents: await fs.promises.readFile(args.path),
+//       loader: "binary",
+//     }));
+//   },
+// };
 
 /** @type {import("esbuild").BuildOptions} */
 const buildOptions = {
   entryPoints: ["src/index.ts"],
   outdir: "dist",
   assetNames: "[name][ext]",
-  platform: "browser",
-  target: "es2020",
+  platform: "node",
+  mainFields: ["main"],
   format: "esm",
   sourcemap: true,
   bundle: true,
   plugins: [
-    wasmPlugin
+    wasmLoader()
   ],
   minify: true,
 };
