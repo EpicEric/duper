185f7bb14744d901822098fee53abd4c90f52093
Eric Rodrigues Pires <eric@eric.dev.br>
2025-11-17 07:35:25 -0300

    Don't bundle wasm-bindgen files

--- duper-js-wasm/esbuild.js
+++ duper-js-wasm/esbuild.js
@@ -1,41 +1,16 @@
 import esbuild from "esbuild";
-import { wasmLoader } from "esbuild-plugin-wasm";
-
-// // https://github.com/evanw/esbuild/issues/408#issuecomment-757555771
-// import path from "node:path"
-// import fs from "node:fs"
-// const wasmPlugin = {
-//   name: "wasm",
-//   setup(build) {
-//     build.onResolve({ filter: /\.wasm$/ }, args => {
-//       if (args.resolveDir === "") {
-//         return;
-//       }
-//       return {
-//         path: path.isAbsolute(args.path) ? args.path : path.join(args.resolveDir, args.path),
-//         namespace: "wasm-binary",
-//       };
-//     })
-//     build.onLoad({ filter: /.*/, namespace: "wasm-binary" }, async (args) => ({
-//       contents: await fs.promises.readFile(args.path),
-//       loader: "binary",
-//     }));
-//   },
-// };
 
 /** @type {import("esbuild").BuildOptions} */
 const buildOptions = {
   entryPoints: ["src/index.ts"],
   outdir: "dist",
   assetNames: "[name][ext]",
-  platform: "node",
+  platform: "neutral",
+  external: ["./wasm-bindgen/index.js"],
   mainFields: ["main"],
   format: "esm",
-  sourcemap: true,
   bundle: true,
-  plugins: [
-    wasmLoader()
-  ],
+  plugins: [],
   minify: true,
 };
 
