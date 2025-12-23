f9ea0f7933825c25f56a5236c13f49e05499a140
Eric Rodrigues Pires <eric@eric.dev.br>
2025-12-23 11:41:48 -0300

    Add Nix flake and improve workspace

---
+++ flake.nix
@@ -0,0 +1,188 @@
+{
+  description = "The format that's super!";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      crane,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        inherit (pkgs) lib;
+
+        craneLib = crane.mkLib pkgs;
+
+        src = lib.fileset.toSource {
+          root = ./.;
+          fileset = lib.fileset.unions [
+            ./Cargo.toml
+            ./Cargo.lock
+            (craneLib.fileset.commonCargoSources ./axum_duper)
+            (craneLib.fileset.commonCargoSources ./duper)
+            (craneLib.fileset.commonCargoSources ./duper-js-node)
+            (craneLib.fileset.commonCargoSources ./duper-js-wasm/rust)
+            (craneLib.fileset.commonCargoSources ./duper-python)
+            (craneLib.fileset.commonCargoSources ./duper_lsp)
+            (craneLib.fileset.commonCargoSources ./duper_uniffi)
+            (craneLib.fileset.commonCargoSources ./duper_website)
+            (craneLib.fileset.commonCargoSources ./duperfmt)
+            (craneLib.fileset.commonCargoSources ./duperq)
+            (craneLib.fileset.commonCargoSources ./serde_duper)
+            (craneLib.fileset.commonCargoSources ./serde_duper_macros)
+            (craneLib.fileset.commonCargoSources ./tracing_duper)
+            (craneLib.fileset.commonCargoSources ./tree-sitter-duper)
+            (lib.fileset.fileFilter (file: file.hasExt "md") ./.)
+            ./.cargo/config.toml
+            ./duperfmt/src/duper.scm
+            ./duperq/tests/data
+            ./duper_uniffi/src/duper.udl
+            ./tree-sitter-duper/src
+            ./tree-sitter-duper/queries
+          ];
+        };
+
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+          version = "0";
+          pname = "duper";
+
+          nativeBuildInputs = with pkgs; [
+            cmake
+            llvmPackages.bintools
+            python3
+          ];
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        individualCrateArgs = commonArgs // {
+          inherit cargoArtifacts;
+          doCheck = false;
+        };
+
+        duperfmt = craneLib.buildPackage (
+          individualCrateArgs
+          // {
+            inherit (craneLib.crateNameFromCargoToml { cargoToml = ./duperfmt/Cargo.toml; }) pname version;
+            cargoExtraArgs = "-p duperfmt";
+          }
+        );
+
+        duperq = craneLib.buildPackage (
+          individualCrateArgs
+          // {
+            inherit (craneLib.crateNameFromCargoToml { cargoToml = ./duperq/Cargo.toml; }) pname version;
+            cargoExtraArgs = "-p duperq";
+          }
+        );
+
+        duper_lsp = craneLib.buildPackage (
+          individualCrateArgs
+          // {
+            inherit (craneLib.crateNameFromCargoToml { cargoToml = ./duper_lsp/Cargo.toml; }) pname version;
+            cargoExtraArgs = "-p duper_lsp";
+          }
+        );
+      in
+      {
+        packages = {
+          inherit duperfmt duperq duper_lsp;
+        };
+
+        apps = {
+          duperfmt =
+            (flake-utils.lib.mkApp {
+              drv = duperfmt;
+            })
+            // {
+              meta = {
+                description = "The official Duper formatting library and CLI";
+                homepage = "https://duper.dev.br";
+                license = lib.licenses.mit;
+                mainProgram = "sandhole";
+                platforms = lib.platforms.linux ++ lib.platforms.darwin;
+              };
+            };
+          duperq =
+            (flake-utils.lib.mkApp {
+              drv = duperq;
+            })
+            // {
+              meta = {
+                description = "A fast Duper and JSON filter/processor";
+                homepage = "https://duper.dev.br";
+                license = lib.licenses.mit;
+                mainProgram = "sandhole";
+                platforms = lib.platforms.linux ++ lib.platforms.darwin;
+              };
+            };
+          duper_lsp =
+            (flake-utils.lib.mkApp {
+              drv = duper_lsp;
+            })
+            // {
+              meta = {
+                description = "The official Duper language server, with auto-formatting and diagnostics";
+                homepage = "https://duper.dev.br";
+                license = lib.licenses.mit;
+                mainProgram = "sandhole";
+                platforms = lib.platforms.linux ++ lib.platforms.darwin;
+              };
+            };
+        };
+
+        checks = {
+          duper-clippy = craneLib.cargoClippy (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+            }
+          );
+
+          duper-doc = craneLib.cargoDoc (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+            }
+          );
+
+          duper-fmt = craneLib.cargoFmt (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+            }
+          );
+        };
+
+        devShells.default = craneLib.devShell {
+          checks = self.checks.${system};
+
+          packages = [
+            pkgs.binaryen
+            pkgs.cargo-rail
+            pkgs.dotnet-sdk_8
+            pkgs.jdk21_headless
+            pkgs.just
+            pkgs.llvmPackages.bintools
+            pkgs.nodejs_24
+            pkgs.tree-sitter
+            pkgs.uv
+            pkgs.wasm-bindgen-cli_0_2_100
+          ];
+        };
+      }
+    );
+}
